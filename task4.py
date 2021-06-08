680c2a94efe66ba4a03a9fdb731ed72586e6df68
EnagantiKarunKumar <75234160+EnagantiKarunKumar@users.noreply.github.com>
2021-06-08 17:55:00 +0530

    Create task4.py

---
+++ task4.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+# Prepare the data
+x = np.linspace(0, 100, 100)
+y=np.linspace(0,100,100)
+# Plot the data
+plt.plot(x, y, label='linear')
+# Show the plot
+plt.show()
