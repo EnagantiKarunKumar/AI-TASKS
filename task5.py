ec8f43e7e6791587f28fd863383063fd5fe453c6
EnagantiKarunKumar <75234160+EnagantiKarunKumar@users.noreply.github.com>
2021-06-08 17:57:32 +0530

    Create task5.py

---
+++ task5.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import math
+import numpy as np
+
+x = open("file.txt", "r")
+x = x.read()
+x = x.split(" ")
+for i in range(100):
+    x[i] = int(x[i])
+y = list(range(1, 101))
+for i in range(1, 99):
+    y[i] = x[i] ** 2 + math.log(x[i] / 2)
+
+# Plot the data
+plt.plot(x, y, label='graph')
+
+# Show the plot
+plt.show()
